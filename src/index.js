373ab485f8fa5f965c02b94569796c61ad49f499
CodyC <codyicastro@gmail.com>
2020-08-25 16:01:41 -0400

    deliverables with Jim

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+  
   // your code here
+
+  const taskForm = document.getElementById('create-task-form');
+
+  let userInput = taskForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+   console.log(event.target[0].value);
+
+const deleteButton = document.getElementById('tasks');
+deleteButton.addEventListener('click', function(event) {
+  h1.remove();
+  console.log("event is gone!")
+}
+
+)
+
+const taskLi = document.createElement("li")
+taskLi.classList.add("task")
+taskLi.innerHTML = `<h1> ${event.target[0].value}  </h1> <button>x</button>`
+ let taskList = document.querySelector("#tasks")
+tasks.append(taskLi)
+
+  });
+
 });
